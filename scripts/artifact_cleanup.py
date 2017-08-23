2d36a257c5462c548d0e354e672b432562fd4bb4
apaul@transparent.com <apaul@transparent.com>
2017-08-23 09:13:38 -0400

    smarter package management

--- scripts/artifact_cleanup.py
+++ scripts/artifact_cleanup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 
-import ast
+from natsort import natsorted
 import cStringIO
+import json
 import os
 import pycurl
 
+
 arturl = os.environ['ARTIFACTORY_URL']
 artbase = os.environ['BASE_REPO']
-artcount = os.environ['BASE_ARTIFACT_COUNT']
+artcount = int(os.environ['BASE_ARTIFACT_COUNT'])
 artgroup = os.environ['ARTIFACT_GROUP']
-artname = os.environ['ARTIFACT_NAME']
 artuser = os.environ['ARTIFACTORY_USER']
 artpass = os.environ['ARTIFACTORY_PASS']
 artapi = arturl + '/api'
 
 
 def delete_artifact(artpath):
-    artifact_url = "%s/%s" % (arturl, artpath)
+    artifact_url = "%s/%s%s" % (arturl, artgroup, artpath)
     print "In queue to be deleted: " + artifact_url
     c = pycurl.Curl()
     c.setopt(pycurl.USERNAME, artuser)
@@ -30,16 +31,19 @@ def delete_artifact(artpath):
 def list_of_artifacts_to_delete():
     response = cStringIO.StringIO()
     c = pycurl.Curl()
-    c.setopt(pycurl.URL, "%s/search/artifact?name=%s&repos=%s" % (artapi, artname, artbase))
+    c.setopt(pycurl.URL, "%s/storage/%s/%s" % (artapi, artbase, artgroup))
     c.setopt(pycurl.CUSTOMREQUEST, 'GET')
     c.setopt(pycurl.WRITEFUNCTION, response.write)
     c.perform()
     c.close()
-    list_of_artifacts = ast.literal_eval(response.getvalue())
-    total_artifacts = len(list_of_artifacts['results'])
-
+    data = json.loads(response.getvalue())
+    artifacts_list = list()
+    for artifact in data['children']:
+        artifacts_list.append(str(artifact['uri']))
+    total_artifacts = len(artifacts_list)
+    artifacts_list = natsorted(artifacts_list) # Perform a natural sort
     if total_artifacts > artcount:
         target = (total_artifacts - artcount) - 1  # Get the list index
-        delete_artifact(list_of_artifacts['results'][target]['uri'].split("storage/")[1])
+        delete_artifact(artifacts_list[target])
 
 list_of_artifacts_to_delete()
